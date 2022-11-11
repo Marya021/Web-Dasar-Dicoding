ab24cc468719f8fa796ff862a861228f903db1e9
MaryaBatubara <maryabatubara0202@gmail.com>
2022-11-11 11:08:31 +0700

    be studying

---
+++ test1.js
@@ -0,0 +1,26 @@
+var firstName = "harry";
+console.log(firstName);
+
+firstName ="Ron";
+console.log(firstName);
+
+
+var x;
+x=100;
+console.log(x);
+
+let x; // x merupakan undefined
+x = 1 // sekarang x merupakan number
+x = true // sekarang x merupakan boolean
+x = "Harry" // sekarang x merupakan string
+
+let user = {
+    firstName: "Harry",
+    lastName: "Potter", 
+    age: 20, 
+    isMuggle: false,
+    stuff: ["Magic Wind", "Flying Car", "Owl"]
+};
+
+console.log("Hallo, nama saya " + user.firstName + " " + user.lastName);
+console.log("Umur saya " + user.age + " tahun");
